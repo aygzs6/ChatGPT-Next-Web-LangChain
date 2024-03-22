70b9b77e2d2ad9389dea94f37d7ef5fc0c5bf6bd
aygzs6 <153046797+aygzs6@users.noreply.github.com>
2024-03-22 08:33:02 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { GoogleTagManager } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "阿远的GPT",
+  description: "ay的免费AI网站.",
   viewport: {
     width: "device-width",
     initialScale: 1,
